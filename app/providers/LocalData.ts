6da7a320c18adb0d7fb833abdd55833b58a897ae
hugessen <richard@hugessen.ca>
2016-09-18 15:49:31 -0400

    Another change
    Literally git just didn't commit this with the rest.
    Not a real update

--- app/providers/LocalData.ts
+++ app/providers/LocalData.ts
@@ -86,6 +86,7 @@ export class LocalData {
             club:22,
             banner:"img/Event Banners/O-Day.jpg",
             tags: [
+                "Networking","First Year"
             ],
             desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
         {
@@ -98,6 +99,7 @@ export class LocalData {
             club:21,   
             banner:"img/Event Banners/5DaysBanner.jpg",
             tags: [
+               "Philanthropy", "Leadership", "Social" 
             ],
             desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             
